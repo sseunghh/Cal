309d4867a3fc6af38d6844c227bc1eeb8d955935
cocoachou <123175470+cocoachou@users.noreply.github.com>
2024-07-10 10:56:50 +0900

    update %

--- Calculator Pro/Calculator Pro/Form1.cs
+++ Calculator Pro/Calculator Pro/Form1.cs
@@ -202,10 +202,6 @@ namespace Calculator_Pro
 
         }
 
-        private void textBox_output_TextChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void button19_Click(object sender, EventArgs e)
         {
@@ -239,8 +235,11 @@ namespace Calculator_Pro
 
         private void button14_Click(object sender, EventArgs e)
         {
-            textBox_output.Text += textBox_input.Text + "%";
-            textBox_input.Text = "";
+            if (!string.IsNullOrEmpty(textBox_input.Text))
+            {
+                textBox_output.Text += textBox_input.Text + "%";
+                textBox_input.Text = "";
+            }
         }
 
         private void button17_Click(object sender, EventArgs e)
